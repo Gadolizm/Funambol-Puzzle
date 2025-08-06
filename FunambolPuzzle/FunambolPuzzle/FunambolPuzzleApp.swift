0b190bcd7218578fe0f3ca3d5bbee7483c589f60
Haitham Gado <taegado@gmail.com>
2025-08-06 20:53:36 +0300

    Initial Xcode project setup with fallback image asset

---
+++ FunambolPuzzle/FunambolPuzzle/FunambolPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunambolPuzzleApp.swift
+//  FunambolPuzzle
+//
+//  Created by Haitham Gado on 06/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FunambolPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
